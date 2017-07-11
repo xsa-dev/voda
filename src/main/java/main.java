dcdaed9f9a4afa4002b36adc050183e959ef6c11
xsd <saleksey67@gmail.com>
2017-07-11 20:21:12 +0300

    it's worked!

---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by User on 11.07.2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new voda());
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/voda.java
@@ -0,0 +1,42 @@
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by User on 11.07.2017.
+ */
+public class voda extends TelegramLongPollingBot {
+    public voda() {
+        super();
+    }
+
+    public String getBotToken() {
+        return "380086304:AAFlk69S3b1wNSyVHvrIPBCVfLI8zzhaaLg";
+    }
+
+    public void onUpdateReceived(Update update) {
+
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                SendMessage message = new SendMessage();
+                message.setChatId((update.getMessage().getChatId())).setText("hello, alex");
+                sendMessage(message);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public String getBotUsername() {
+        return "voda_bot";
+    }
+
+    public voda(DefaultBotOptions options) {
+        super(options);
+    }
+}
