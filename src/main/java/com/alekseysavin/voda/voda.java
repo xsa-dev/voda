d4d28dc4f7566a82f7b88f10537371db807300d0
Aleksey Savin <savin.av@rusoft-company.ru>
2017-07-19 16:28:57 +0300

    updated, but need some time....

--- src/main/java/com/alekseysavin/voda/voda.java
+++ src/main/java/com/alekseysavin/voda/voda.java
@@ -8,12 +8,17 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 /**
  * Created by User on 11.07.2017.
+ * Updated by User on 19.07.2017.
  */
 public class voda extends TelegramLongPollingBot {
     private long ChatId;
+    private String[] inString;
+    private HashMap<String, Integer> vodaList;
 
     public void onUpdateReceived(Update update) {
 
@@ -31,13 +36,19 @@ public class voda extends TelegramLongPollingBot {
   
                 if (CurrentInMessage.equals("/status")) {
                     message.setChatId(ChatId);
-                    message.setText("Ok, is is status");
+                    message.setText("Ok, is is status;");
+                    //need update
+                    for (Entry<String, Integer> voda : vodaList.entrySet()) {
+						message.setText(voda.getKey() + " " + voda.getValue());
+					}                    
                     sendMessage(message);
-                } else
-                    for (String str : messages) {
-                        message.setText(str);
-                        sendMessage(message);
-                    }
+                } else if (CurrentInMessage.startsWith("+")) {
+                	//need update
+                	inString = update.getMessage().getText().split(" ");
+                		for (int i = 0; i < inString.length; i++) {
+                			vodaList.put(inString[1], Integer.parseInt(inString[2]));
+                		}
+                }
             }
         } catch (Exception e) {
         	SendMessage message = new SendMessage();
