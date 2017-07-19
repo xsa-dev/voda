dbf0279fb4ef385519cd506b39231150df588be9
Aleksey Savin <savin.av@rusoft-company.ru>
2017-07-19 16:46:55 +0300

    fix voda_alfa

--- src/main/java/com/alekseysavin/voda/voda.java
+++ src/main/java/com/alekseysavin/voda/voda.java
@@ -1,11 +1,11 @@
 package com.alekseysavin.voda;
 
-import org.telegram.telegrambots.TelegramBotsApi;
+
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 
 import java.util.ArrayList;
 
