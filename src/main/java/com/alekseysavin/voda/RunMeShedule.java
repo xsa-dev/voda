313ef548f9ee955e29571385c9c1031615ff8f9b
xsd <saleksey67@gmail.com>
2017-07-22 10:48:35 +0300

    compile error fix

--- src/main/java/com/alekseysavin/voda/RunMeShedule.java
+++ src/main/java/com/alekseysavin/voda/RunMeShedule.java
@@ -2,7 +2,7 @@ package com.alekseysavin.voda;
 
 import java.util.TimerTask;
 import org.telegram.telegrambots.*;
-import sun.plugin2.message.Message;
+
 
 /**
  * Created by xsd on 21.07.2017 with love.
