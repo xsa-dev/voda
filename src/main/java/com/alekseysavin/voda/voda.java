88432a8764d30577523713bb700c8d8acb2737e8
xsd <saleksey67@gmail.com>
2017-08-28 20:45:56 +0300

    updated & minor testing

--- src/main/java/com/alekseysavin/voda/voda.java
+++ src/main/java/com/alekseysavin/voda/voda.java
@@ -1,16 +1,9 @@
 package com.alekseysavin.voda;
 
-
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.TimerTask;
 
 /**
  * Created by User on 11.07.2017
@@ -21,9 +14,9 @@ import java.util.TimerTask;
 public class voda extends TelegramLongPollingBot {
 
     int bSum = 0; // белок
-    int kSum = 0; // калории
-    int pSum = 0; // клетчатка
-    int vSum = 0; // вода
+    int eSum = 0; // энергия
+    int vSum = 0; // клетчатка
+    int wSum = 0; // вода
     int aSum = 0; // активность
 
     String[] vStringArray;
@@ -46,67 +39,93 @@ public class voda extends TelegramLongPollingBot {
                 SendMessage message = new SendMessage();
                 message.setChatId(ChatId);
 
+                if (CurrentInMessage.equals("/help")) {
+                    message.setChatId(ChatId);
+                    String helpText = "Как пользоваться:" + "\n"
+                            + "Строка: + хлеб 10б 150э 5в - добавит 10 белка, 150 калорий, 5 пищевых волокон" +  "\n"
+                            + "Строка: * вода 500 - добавит 500 мл воды (кофе и чай - не вода, нужно выпить столько же!)" + "\n"
+                            + "& 500 - Активность" + "\n"
+                            + "% 1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9 - Замеры";
+
+                    message.setText(helpText);
+                    message.setChatId(ChatId);
+                    sendMessage(message);
+                }
                 if (CurrentInMessage.equals("/results")) {
                     message.setChatId(ChatId);
                     String outMesasge = "Результаты:" + "\n";
+                    message.setText(outMesasge + "\n" + resultString);
 
-                    message.setText(outMesasge + "\n" + resultString;
-
-                    )
                 }
                 if (CurrentInMessage.equals("/status")) {
-
                     message.setChatId(ChatId);
-
                     String outMessage = "Дневник: " + "\n";
-
                     message.setText(outMessage + "\n"
                             + "Белка за день: " + bSum + "\n"
-                            + "Калории: " + kSum + "\n"
-                            + "П.В.: " + pSum + "\n"
-                            + "Воды: " + vSum + "\n"
+                            + "Энергия: " + eSum + "\n"
+                            + "П.В.: " + vSum + "\n"
+                            + "Воды: " + wSum + "\n"
                             + "Активность: " + aSum + "\n"
                     );
-
                     sendMessage(message);
-
                 } else if (CurrentInMessage.startsWith("*")) {
-
                     message.setChatId(ChatId);
                     System.out.println(update.getMessage().getText());
                     vStringArray = update.getMessage().getText().split(" ");
-                    vSum += Integer.parseInt(vStringArray[1]); // добавляем воду
-
+                        try {
+                            wSum += Integer.parseInt(vStringArray[2]); // добавляем воду
+                        }
+                        catch (NumberFormatException nfe) {
+                            System.out.println(nfe);
+                            String error = nfe.toString();
+                            message.setText("Не понял." + "\n" + error);
+                            message.setChatId(ChatId);
+                            sendMessage(message);
+                        }
                 } else if (CurrentInMessage.startsWith("+")) {
-
                     message.setChatId(ChatId);
+                    System.out.println(update.getMessage().getText());
                     eStringArray = update.getMessage().getText().split(" ");
-
-                    for (int i = 2; i < eStringArray.length; i++) {
-                        if (eStringArray[i].endsWith("б")) {
-                            String string = eStringArray[i].toString();
-                            bSum = Integer.parseInt(string.replace("б", ""));
+                    try {
+                        for (int i = 2; i < eStringArray.length; i++) {
+                            if (eStringArray[i].endsWith("б")) {
+                                String string = eStringArray[i].toString();
+                                bSum += Integer.parseInt(string.replace("п", ""));
+                            }
+                            if (eStringArray[i].contains("э")) {
+                                String string = eStringArray[i].toString();
+                                eSum += Integer.parseInt(string.replace("э", ""));
+                            }
+                            if (eStringArray[i].endsWith("в")) {
+                                String string = eStringArray[i].toString();
+                                vSum = Integer.parseInt(string.replace("в", ""));
+                            }
                         }
-                        if (eStringArray[i].contains("к")) {
-                            String string = eStringArray[i].toString();
-                            kSum += Integer.parseInt(string.replace("к", ""));
-                        }
-                        if (eStringArray[i].endsWith("п")) {
-                            String string = eStringArray[i].toString();
-                            pSum = Integer.parseInt(string.replace("п", ""));
-                        }
+                    } catch (NumberFormatException nfe) {
+                        System.out.println(nfe.toString());
+                        String error = nfe.toString();
+                        message.setText("Не понял." + "\n" + error);
+                        message.setChatId(ChatId);
+                        sendMessage(message);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        String error = e.toString();
+                        message.setText("Не разобрал что ты написал." + "\n" + error);
+                        message.setChatId(ChatId);
+                        sendMessage(message);
                     }
-                } else if (CurrentInMessage.startsWith("@")) {
+                } else if (CurrentInMessage.startsWith("&")) {
                     message.setChatId(ChatId);
+                    System.out.println(update.getMessage().getText());
                     aStringArray = update.getMessage().getText().split(" ");
-                    aSum += Integer.parseInt(aStringArray[1]); // добавляем воду
-                } else if (CurrentInMessage.startsWith(":")) {
+                    aSum += Integer.parseInt(aStringArray[1]); // добавляем активность
+                } else if (CurrentInMessage.startsWith("%")) {
 
                     rStringArray = update.getMessage().getText().split(" ");
 
                     Result result = new Result(
 
-                            Float.parseFloat(rStringArray[0].replace(":","")),
+                            Float.parseFloat(rStringArray[0].replace("%","")),
                             Float.parseFloat(rStringArray[1]),
                             Float.parseFloat(rStringArray[2]),
                             Float.parseFloat(rStringArray[3]),
@@ -116,7 +135,6 @@ public class voda extends TelegramLongPollingBot {
                             Integer.parseInt(rStringArray[7]),
                             Integer.parseInt(rStringArray[8]),
                             Float.parseFloat(rStringArray[9]));
-
                     resultString = result.toString();
                     System.out.println(result.toString());
                 }
