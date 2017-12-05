3fceb21f1782fbad7c7bcf9ef3929b5d54421d8e
Aleksey Savin <saleksey67@gmail.com>
2017-12-05 12:49:27 +0300

    maven support added

--- src/main/java/com/AppMainClass.java
+++ src/main/java/com/AppMainClass.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 
 public class AppMainClass {
 
-    public static void vodaStart(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         //dbmodel.MysqlCon conn = new dbmodel.MysqlCon();
 
