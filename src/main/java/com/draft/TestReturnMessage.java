0a15937e5d96cf8f9a261dbcf523eff4c55d1efe
Aleksey Savin <saleksey67@gmail.com>
2017-12-10 16:53:30 +0300

    fix

--- src/main/java/com/draft/TestReturnMessage.java
+++ src/main/java/com/draft/TestReturnMessage.java
@@ -2,6 +2,6 @@ package com.draft;
 
 public class TestReturnMessage {
     public String getTestRetunMessage() {
-        return "This is return Message for answer to user from bot from commant test Calculate";
+        return "This is return Message for answer to user from bot from command test Calculate";
     }
 }
